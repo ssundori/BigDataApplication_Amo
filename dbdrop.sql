eb8ba7e288cd9b49cecdba4555770d7c5f01f76e
ssundori <snn0814@ewhain.net>
2025-11-11 23:54:45 +0900

    6~8 테이블 삽입

---
+++ dbdrop.sql
@@ -0,0 +1,17 @@
+SET FOREIGN_KEY_CHECKS = 0;
+
+DROP TABLE user_insert;
+DROP TABLE country_monthly_temperatures;
+DROP TABLE disasters;
+
+DROP TABLE country;
+DROP TABLE disaster_types;
+
+DROP TABLE continent;
+DROP TABLE user_info;
+
+DROP TABLE co2_concentration;
+DROP TABLE global_annual_sst_anomalies;
+DROP TABLE global_annual_temperatures;
+
+SET FOREIGN_KEY_CHECKS = 1;
