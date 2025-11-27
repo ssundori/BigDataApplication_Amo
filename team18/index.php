bc7437eb6fc2b30da1b3702de5ae2bd406f646c3
daisy810 <dahye.kang03@gmail.com>
2025-11-28 00:03:00 +0900

    windowing 수정

--- team18/index.php
+++ team18/index.php
@@ -9,6 +9,7 @@
     <link rel="stylesheet" href="./fe/css/sidebar.css">
     
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 </head>
 <body>
     <aside id="sidebar-container" class="sidebar">
