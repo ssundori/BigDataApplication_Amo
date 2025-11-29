0439f42074f62accccd0ae22e2487e7a14c52389
yujung9082 <144993353+yujung9082@users.noreply.github.com>
2025-11-29 15:47:05 +0900

    Update db.php 포트번호 수정

--- team18/api/config/db.php
+++ team18/api/config/db.php
@@ -2,7 +2,7 @@
 mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 
 $host = "127.0.0.1";
-$port = 3307;
+$port = 3306;
 $user = "team18";
 $pass = "team18";
 $dbname = "team18";
@@ -19,4 +19,5 @@ try {
     ]);
     exit;
 }
+
 
