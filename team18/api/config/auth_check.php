93aa48b9297db1c3373a06244f6e98c5bbb2da30
yujung9082 <144993353+yujung9082@users.noreply.github.com>
2025-11-29 09:39:14 +0900

    Update auth_check.php 401대신 로그인 요구 문구 띄우기

--- team18/api/config/auth_check.php
+++ team18/api/config/auth_check.php
@@ -6,10 +6,11 @@ if (session_status() === PHP_SESSION_NONE) {
 
 // 세션에 로그인 정보 없는 경우
 if (!isset($_SESSION["user_id"])) {
-    http_response_code(401);
-    echo json_encode([
-        "success" => false,
-        "message" => "Login required."
-    ]);
+    echo "
+        <script>
+            alert('로그인 후 이용해주세요.');
+            window.location.href = '/team18/login.php';
+        </script>
+    ";
     exit;
 }
